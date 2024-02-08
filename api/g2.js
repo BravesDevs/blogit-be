872330085d326b64ff3a4561ddde47ee8c4ab440
Devang MP <devangparekh2014@gmail.com>
2024-02-08 17:27:55 -0500

    Adding validations

--- api/g2.js
+++ api/g2.js
@@ -3,7 +3,30 @@ const g2RegisterAPI = async (req, res, next) => {
   const { body } = req;
 
   try {
-    const { g2 } = await g2RegisterService(body);
+    const { firstName, lastName, licenseNo, age, make, model, year, platNo } =
+      body;
+
+    if (
+      !firstName ||
+      !lastName ||
+      !licenseNo ||
+      !age ||
+      !make ||
+      !model ||
+      !year ||
+      !platNo
+    ) {
+      res.status(400).json({ message: "All fields are required" });
+    }
+
+    const data = {
+      firstName,
+      lastName,
+      licenseNo,
+      carDetails: { age, make, model, year, platNo },
+    };
+
+    const { g2 } = await g2RegisterService(data);
     res.status(201).json(g2);
   } catch (error) {
     next(error);
